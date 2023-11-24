ecc43eb944e3d496418193e3cfc2e2155c075ab0
Genilson Souza <genilsonsouza.dev@gmail.com>
2023-11-24 14:19:00 -0300

    sliders e SEO

--- assets/js/keto.js
+++ assets/js/keto.js
@@ -1,3 +1,9 @@
+import Slider from './Slider.js'
+
+
+
+
+/* 
 async function getReviews() {
     var placeId = 'ChIJx10QDzL2zpQROVoah2YT61I'
     var path = `./assets/json/keto.json`
@@ -32,22 +38,63 @@ async function getReviews() {
     }
 
 }
+ */
 
-getReviews()
 
-var _ml = document.querySelector('.slider_loop').clientWidth + 24
 
-function next() {
-    var palco = document.querySelector('.slider_loop')
-    var largura = _ml * -1
-    var position_now = +palco.style.getPropertyValue('--ml').replace('px', '')
-    palco.style.setProperty('--ml', (largura + position_now) + "px");
 
+
+
+function renderItem({ largura, gravatar, author, content }) {
+    
+
+	return `
+    <div class="slider_card" >
+        <div style="width: ${largura}px">
+            <div>
+                <img src="${gravatar}" alt="${author}" class="slider_gravatar">
+            </div>
+            <div>
+                <strong style="--cor: #000;">${author}</strong>
+                <span>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                </span>
+            </div>
+        </div>
+        <p style="--cor: #777;">${content}</p>
+    </div>
+    `
 }
 
-function prev() {
-    var palco = document.querySelector('.slider_loop')
-    var largura = _ml
-    var position_now = +palco.style.getPropertyValue('--ml').replace('px', '')
-    palco.style.setProperty('--ml', (largura + position_now) + "px");
-}
+; (async _ => {
+	var placeId = 'ChIJx10QDzL2zpQROVoah2YT61I'
+	var path = `./assets/json/keto.json`
+	var data = await (await fetch(path)).json()    
+
+	const options = {
+		btnNext: [document.querySelector('.js-next')],
+		btnPrev: [document.querySelector('.js-prev')],
+		steps: [document.querySelector('.js-steps')],
+		gap: 20,
+		data: data['data'],
+		item: renderItem,
+		breakPoints: {
+			0: 1,
+			420: 2,
+			768: 3,
+		}
+		,
+		autoPlay: true,
+		delay: 3000
+	}
+	const $slider = document.querySelector('.js-reviews')
+
+    console.log($slider)
+
+	const sl = new Slider($slider, options)
+
+})()
